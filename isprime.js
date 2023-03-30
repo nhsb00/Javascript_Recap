932178d7d4f4f472c582ad3e3c003932d5eb1643
nhsb00 <nhsb00@hotmail.com>
2023-03-29 23:35:46 -0400

    primeNumer

--- isprime.js
+++ isprime.js
@@ -2,7 +2,9 @@ const isPrime = (num) => {
     if (num < 2) return false;
 
     for  (let i = 2; i <= Math.sqrt(num); i += 1) {
-        if (n % i === 0) return false;
+        if (num % i === 0) {
+            return false;
+        }
     }
 
     return true;
@@ -12,4 +14,18 @@ const isPrime = (num) => {
 // Time: O(square_root(n))
 // Space: O(1)
 
-console.log(isPrime(2));
+const primeNums = (num) => {
+    let primeNum = [];
+    for (let i = 2; i < num; i += 1) {
+        if (isPrime(i)) {
+            primeNum.push(i);
+        }
+    }
+
+    return primeNum;
+}
+
+console.log(primeNums(2));
+console.log(primeNums(3));
+console.log(primeNums(13));
+console.log(primeNums(100));
