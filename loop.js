b5cdff4a9d7dc526c3e59a999a700f39262478b3
nhsb00 <nhsb00@hotmail.com>
2023-04-22 05:32:29 -0400

    loop

---
+++ loop.js
@@ -0,0 +1,48 @@
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     sum += i
+// }
+
+// console.log(sum);
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+
+// for (let i = 1; i <= 9; i++) {
+//     console.log(`${n} * ${i} = ${n * i}`)
+// }
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let result = "";
+
+// for (let i = 0; i < n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
+// let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// let n = Number(input[0]);
+// let sum = '';
+// for (let t = 1; t <= n; t++) {
+//     let data = input[t].split(' ');
+//     let a = Number(data[0]);
+//     let b = Number(data[1]);
+//     sum += a + b + '\n';
+// }
+// console.log(sum);
