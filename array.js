7f993ebed5fd072a86c05f9be0060e358cdef917
nhsb00 <nhsb00@hotmail.com>
2023-05-19 03:12:45 -0400

    charAt and repeat

--- array.js
+++ array.js
@@ -34,3 +34,18 @@ for (let x of string) {
     answer += Number(x);
 }
 console.log(answer);
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let testCase = Number(input[0]);
+
+for (let i = 1; i <= testCase; i++) {
+    let [r,s] = input[i].split(" ");
+    let result = "";
+
+    for (let j = 0; j <= s.length; j++) {
+        result += s.charAt(j).repeat(r);
+    }
+    console.log(result);
+}
