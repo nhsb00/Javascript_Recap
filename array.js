ce9186ca9dbf668b797296e21e83b4d137af2e3c
nhsb00 <nhsb00@hotmail.com>
2023-05-19 03:29:21 -0400

    group word check

--- array.js
+++ array.js
@@ -57,3 +57,28 @@ let b = input[0].split(' ')[1];
 let newA = a[2] + a[1] + a[0];
 let newB = b[2] + b[1] + b[0];
 console.log(Math.max(Number(newA), Number(newB)));
+
+function check(data) {
+    let setData = new Set(data[0]);
+    for (let i = 0; i < data.length - 1; i++) {
+        if (data[i] != data[i + 1]) {
+            if (setData.has(data[i + 1])) {
+                return false;
+            }
+            else {
+                setData.add(data[i + 1]);
+            }
+        }
+    }
+    return true;
+}
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let n = Number(input[0]);
+let summary = 0;
+for (let i = 1; i <= n; i++) {
+    let data = input[i];
+    if (check(data)) summary += 1
+}
+console.log(summary);
