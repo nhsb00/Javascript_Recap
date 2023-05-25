3d81586cd3e4885b4e766ef833e0dc46f1973fd4
nhsb00 <nhsb00@hotmail.com>
2023-05-25 05:14:23 -0400

    tribonacci without memorization timeout

--- structy/tribonacci.js
+++ structy/tribonacci.js
@@ -1,3 +1,13 @@
 const tribonacci = (n) => {
- 
+    if (n === 0 || n === 1) return 0;
+
+    if (n === 2) return 1;
+
+    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
 };
+
+console.log(tribonacci(1));
+console.log(tribonacci(3));
+console.log(tribonacci(5));
+console.log(tribonacci(9));
+console.log(tribonacci(46));
