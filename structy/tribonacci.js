f4db5368f74a37549b0e8f09402aafc7f225b523
nhsb00 <nhsb00@hotmail.com>
2023-05-25 10:12:39 -0400

    tribonacci memorization

--- structy/tribonacci.js
+++ structy/tribonacci.js
@@ -1,10 +1,19 @@
-const tribonacci = (n) => {
-    if (n === 0 || n === 1) return 0;
+// const tribonacci = (n) => {
+//     if (n === 0 || n === 1) return 0;
 
-    if (n === 2) return 1;
+//     if (n === 2) return 1;
 
-    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+//     return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+// };
+
+const tribonacci = (n, memo = {}) => {
+    if (n === 0 || n === 1) return 0
+    if (n === 2) return 1
+    if (n in memo) return memo[n]
+
+    return memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo)
 };
+
 
 console.log(tribonacci(1));
 console.log(tribonacci(3));
