0ddc2ff36e975f8b9fe7fbba0e1159e156ea7f31
nhsb00 <nhsb00@hotmail.com>
2023-06-01 08:19:23 -0400

    dynamic programming without memorization occured time out

--- structy/sumPossible.js
+++ structy/sumPossible.js
@@ -1,10 +1,21 @@
-const sumPossible = (amount, numbers, memo = {}) => {
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = 0; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === amount) {
-                return true
-            }
+const sumPossible = (amount, numbers) => {
+    // for (let i = 0; i < numbers.length; i++) {
+    //     for (let j = 0; j < numbers.length; j++) {
+    //         if (numbers[i] + numbers[j] === amount) {
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false;
+    if (amount === 0) return true;
+
+    if (amount < 0) return false;
+
+    for (let num of numbers) {
+        if (sumPossible(amount - num, numbers) === true) {
+            return true;
         }
     }
+
     return false;
 }
