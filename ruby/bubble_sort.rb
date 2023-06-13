57e4ccb2d88bac4ce9f1f82c5786bdf9dbf3094e
nhsb00 <nhsb00@hotmail.com>
2023-06-13 10:17:41 -0400

    ruby nested loop bubble sort

--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -1,17 +1,31 @@
+# def bubble_sort(arr)
+#     sorted = false
+
+#     while !sorted
+#         sorted = true
+#         (0...arr.length - 1).each do |i|
+#             if arr[i] > arr[i + 1] #if the adj elements are OUT OF ORDER
+#                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
+#                 sorted = false
+#             end
+#         end
+#     end
+
+#     arr
+# end
+
 def bubble_sort(arr)
-    sorted = false
+  length = arr.length
 
-    while !sorted
-        sorted = true
-        (0...arr.length - 1).each do |i|
-            if arr[i] > arr[i + 1] #if the adj elements are OUT OF ORDER
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                sorted = false
-            end
-        end
+  (0..length-2).each do |i|
+    (0..(length-i-2)).each do |j|
+      if arr[j] > arr[j+1]
+        arr[j+1], arr[j] = arr[j], arr[j+1]
+      end
     end
+  end
 
-    arr
+  arr
 end
 
 arr = [2, 8, 5, 2, 6]
