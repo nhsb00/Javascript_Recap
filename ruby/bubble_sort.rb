ad0b2cc317f2e09029d176fffd9decd087cdac8d
nhsb00 <nhsb00@hotmail.com>
2023-06-13 09:05:32 -0400

    fully sorted array using while loop is true or false

--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -1,7 +1,13 @@
 def bubble_sort(arr)
-    (0...arr.length - 1).each do |i|
-        if arr[i] > arr[i + 1]
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    sorted = false
+
+    while !sorted
+        sorted = true
+        (0...arr.length - 1).each do |i|
+            if arr[i] > arr[i + 1] #if the adj elements are OUT OF ORDER
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                sorted = false
+            end
         end
     end
 
