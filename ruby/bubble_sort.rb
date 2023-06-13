5bbe6386c2b5534871dc53c4be1d2cbd3752495c
nhsb00 <nhsb00@hotmail.com>
2023-06-13 09:01:47 -0400

    not complete bubble sort

--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -1,4 +1,12 @@
 def bubble_sort(arr)
+    (0...arr.length - 1).each do |i|
+        if arr[i] > arr[i + 1]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        end
+    end
 
-    
+    arr
 end
+
+arr = [2, 8, 5, 2, 6]
+p bubble_sort(arr)
