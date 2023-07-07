9b4e1c1fc8c8c0eeaf6436476e873c589fd853fb
nhsb00 <nhsb00@hotmail.com>
2023-07-07 04:12:14 -0400

    bubble sort

--- ruby/algorithmic_strategies/03_bubble_sort.rb
+++ ruby/algorithmic_strategies/03_bubble_sort.rb
@@ -5,7 +5,17 @@
 
 def bubble_sort(arr)
     sorted = false
-    
+
+    while !sorted
+        sorted = true
+        (0...arr.length - 1).each do |i|
+            if arr[i] > arr[i + 1]
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                sorted = false
+            end
+        end
+    end
+    arr
 end
 
 
