e750a5935bd5993e08eb89b53c28b9c4f65bb9ce
nhsb00 <nhsb00@hotmail.com>
2023-07-07 04:09:01 -0400

    bubble sort ruby again

--- ruby/algorithmic_strategies/03_bubble_sort.rb
+++ ruby/algorithmic_strategies/03_bubble_sort.rb
@@ -3,6 +3,11 @@
 # The method should return the array.
 # Do not use the built-in Array#sort
 
+def bubble_sort(arr)
+    sorted = false
+    
+end
+
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
