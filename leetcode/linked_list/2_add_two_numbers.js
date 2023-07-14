d9a31045d78dc8b4e28f9548c260b6a7974b79e6
nhsb00 <nhsb00@hotmail.com>
2023-07-14 07:49:11 -0400

    handle carried overs

--- leetcode/linked_list/2_add_two_numbers.js
+++ leetcode/linked_list/2_add_two_numbers.js
@@ -2,17 +2,37 @@
 // The digits are stored in reverse order, and each of their nodes contains a single digit.
 // Add the two numbers and return the sum as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+class ListNode {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    };
+}; 
+
 var addTwoNumbers = function (l1, l2) {
-    while(l1 || l2) {
+    let carry = 0;
+  
+    while(l1 || l2 || carry) {
     // while either l1 is not null or l2 is not null        
     // l1 or l2 could be null before applying update condition we need if statement to set next node
+        let val1 = 0;
+        let val2 = 0;
+        // save value of l1 and l2
         if (l1) {
+            val1 = l1.val;
             l1 = l1.next;
         }
         if (l2) {
+            val2 = l2.val;
             l2 = l2.next;
         }
+        let sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);//sum > 9 ? 1 : 0 handle carried over
+        let digit = sum % 10;
+       
     }
+    return headNode.next;
 };
 
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]))
