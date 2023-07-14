384f90ed5faa4547958d5e1949db5376d3ebe697
nhsb00 <nhsb00@hotmail.com>
2023-07-14 07:21:36 -0400

    loop until l1 or l2 is empty

--- leetcode/linked_list/2_add_two_numbers.js
+++ leetcode/linked_list/2_add_two_numbers.js
@@ -3,7 +3,9 @@
 // Add the two numbers and return the sum as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 var addTwoNumbers = function (l1, l2) {
-
+    while(l1 || l2) {
+    // while either l1 is not null or l2 is not null        
+    }
 };
 
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]))
